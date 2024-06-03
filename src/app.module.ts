517972f20b3310eedc5b6f72e40df4a78f98c11e
rafi201505061 <rakibul.rafi@revechat.com>
2024-06-03 08:51:30 +0600

    db module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [DatabaseModule, ProductsModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
